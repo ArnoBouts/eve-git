b931536bee4b1ab247af9ae589927c1807aa4a33
Arnaud Bouts <arnaud.bouts@gmail.com>
2017-02-25 14:38:26 +0100

    add git user config

--- eve.sh
+++ eve.sh
@@ -3,6 +3,8 @@
 mkdir -p /tmp/repo
 pwd=$(pwd)
 cd /tmp/repo
+git config --global user.email "${GIT_USER_EMAIL}"
+git config --global user.name "${GIT_USER_NAME}"
 git init
 git remote add origin $1
 sha=$(git rev-parse --short $(git ls-remote origin $2 | cut -f1))

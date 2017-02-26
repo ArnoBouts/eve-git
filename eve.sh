2137d6feb0009d7c15c85f91f803e81e787a3509
Eve <eve@no-cloud.fr>
2017-02-26 16:54:33 +0100

    remove git init output

--- eve.sh
+++ eve.sh
@@ -3,7 +3,7 @@
 mkdir -p /tmp/repo
 pwd=$(pwd)
 cd /tmp/repo
-git init 2>&1 >/dev/null
+git init -q
 git remote add origin ${PLUGIN_REPOSITORY}
 sha=$(git rev-parse --short $(git ls-remote origin ${PLUGIN_BRANCH} | cut -f1))
 cd $pwd

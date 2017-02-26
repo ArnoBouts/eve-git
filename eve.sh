bf1ebc5ff3346d39fa4e74963dc56da586a691ce
Eve <eve@no-cloud.fr>
2017-02-26 17:35:32 +0100

    no more file needed

--- eve.sh
+++ eve.sh
@@ -5,8 +5,15 @@ pwd=$(pwd)
 cd /tmp/repo
 git init -q
 git remote add origin ${PLUGIN_REPOSITORY}
-sha=$(git rev-parse --short $(git ls-remote origin ${PLUGIN_BRANCH} | cut -f1))
+SHA=$(git rev-parse --short $(git ls-remote origin ${PLUGIN_BRANCH} | cut -f1))
 cd $pwd
-echo "${PLUGIN_VARIABLE} $sha"
-sed -i -e "s/${PLUGIN_VARIABLE} .*\$/${PLUGIN_VARIABLE} $sha/1" ${PLUGIN_FILE}
 
+grep -H -o -R "${PLUGIN_VARIABLE} [1-9a-fA-F]*^" * | while read LINE
+do
+	FILE=`echo $LINE | cut -d":" -f1`
+	OLD=`echo $LINE | cut -d" " -f2`
+
+	echo "${FILE} : ${PLUGIN_VARIABLE} : ${OLD} -> ${SHA}"
+	sed -i -e "s/${PLUGIN_VARIABLE} [0-9a-fA-F]*\$/${PLUGIN_VARIABLE} ${SHA}/1" ${FILE}
+
+done

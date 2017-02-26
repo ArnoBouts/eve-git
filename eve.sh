79fed10ea689546d2728cd310cb6b4d0a6d2551d
Eve <eve@no-cloud.fr>
2017-02-26 17:52:28 +0100

    workingdir fix

--- eve.sh
+++ eve.sh
@@ -6,7 +6,8 @@ cd /tmp/repo
 git init -q
 git remote add origin ${PLUGIN_REPOSITORY}
 SHA=$(git rev-parse --short $(git ls-remote origin ${PLUGIN_BRANCH} | cut -f1))
-cd $pwd
+
+cd $pwd/eve
 
 grep -H -o -r "${PLUGIN_VARIABLE} [1-9a-fA-F]*$" * | while read LINE
 do
